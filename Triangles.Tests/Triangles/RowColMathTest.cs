2635733e1f8124aea31534aa541ead0877186059
Kristian Holdhus <45343987+kristianholdhus@users.noreply.github.com>
2018-11-27 10:44:55 -0800

    Add failure tests.

--- Triangles.Tests/Triangles/RowColMathTest.cs
+++ Triangles.Tests/Triangles/RowColMathTest.cs
@@ -7,6 +7,54 @@ namespace Triangles.Triangles
 {
     public class RowColMathTest
     {
+        [Fact]
+        public void FailsWhenTriangleHeightIsLessThanTen()
+        {
+            AssertExceptionForCoordinates("Vertex 1 and 2 are in vertical alignment, but Y coordinates are not 10 appart.",
+                                          v1x: 0, v1y: 0, v2x: 0, v2y: 9, v3x: 10, v3y: 9);
+        }
+
+        [Fact]
+        public void FailsWhenTriangleWidthIsLessThanTen()
+        {
+            AssertExceptionForCoordinates("Vertex 1 and 2 are in horizontal alignment, but Y coordinates are not 10 appart.",
+                                          v1x: 0, v1y: 0, v2x: 9, v2y: 0, v3x: 9, v3y: 10);
+        }
+
+        [Fact]
+        public void FailsWhenDiagonalCoordinatesAreNot10Apart()
+        {
+            AssertExceptionForCoordinates("diagonal where the difference between V1 and V2 must be 10 points",
+                                          v1x: 0, v1y: 0, v2x: 9, v2y: 9, v3x: 9, v3y: 0);
+        }
+
+        [Fact]
+        public void FailsWhenGoodVerticalLegButBadOther()
+        {
+            AssertExceptionForCoordinates("Illegal coordinate configuration where V1 and V2 are in vertical leg.",
+                                          v1x: 0, v1y: 0, v2x: 0, v2y: 10, v3x: 5, v3y: 5);
+        }
+
+        [Fact]
+        public void FailsWhenGoodHorizontalLegButBadOther()
+        {
+            AssertExceptionForCoordinates("Illegal coordinate configuration where V1 and V2 are in horizontal leg.",
+                                          v1x: 0, v1y: 0, v2x: 10, v2y: 0, v3x: 5, v3y: 5);
+        }
+
+        [Fact]
+        public void FailsWhenGoodDiagonalButBadOther()
+        {
+            AssertExceptionForCoordinates("Illegal coordinate configuration where V1 and V2 are in diagonal.",
+                                          v1x: 0, v1y: 0, v2x: 10, v2y: 10, v3x: 5, v3y: 5);
+        }
+
+        private static void AssertExceptionForCoordinates(string messageSubstring, int v1x, int v1y, int v2x, int v2y, int v3x, int v3y)
+        {
+            Exception ex = Assert.Throws<ArgumentOutOfRangeException>(() => RowColMath.GetRowColForCoordinates(v1x, v1y, v2x, v2y, v3x, v3y));
+            Assert.Contains(messageSubstring, ex.Message);
+        }
+
         [Theory]
         [ClassData(typeof(TrianglesByCoordinates))]
         public void ReturnsCorrectCoordinatesForRowAndCol(int v1x, int v1y, int v2x, int v2y, int v3x, int v3y, char expectedRow, int expectedCol)
